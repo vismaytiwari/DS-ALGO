63f14c299a9042c71c28ceaf14e6d4d9afc72266
vismaytiwari <89745751+vismaytiwari@users.noreply.github.com>
2022-06-20 15:29:45 +0530

    Time: 207 ms (72.56%), Space: 14.4 MB (92.09%) - LeetHub

---
+++ 820-short-encoding-of-words/820-short-encoding-of-words.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimumLengthEncoding(self, words):
+        s = set(words)
+        for w in words:
+            for i in range(1, len(w)):
+                s.discard(w[i:])
+        return sum(len(w) + 1 for w in s)
+        
