7d17de3d0784354f7b270979483b05c838da5d49
vismaytiwari <89745751+vismaytiwari@users.noreply.github.com>
2022-08-06 00:03:18 +0530

    Time: 119 ms (52.17%), Space: 14.3 MB (22.51%) - LeetHub

---
+++ 1366-rank-teams-by-votes/1366-rank-teams-by-votes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rankTeams(self, votes: List[str]) -> str:
+        d = {}
+
+        for vote in votes:
+            for i, char in enumerate(vote):
+                if char not in d:
+                    d[char] = [0] * len(vote)
+                d[char][i] += 1
+        voted_names = sorted(d.keys())
+        y = sorted(voted_names, key=lambda x: d[x], reverse=True)
+        return "".join(y)
