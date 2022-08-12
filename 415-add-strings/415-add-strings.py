6c68f539f4e9ffc95b70be7b683e595d50dd2f40
vismaytiwari <89745751+vismaytiwari@users.noreply.github.com>
2022-08-12 10:17:22 +0530

    Time: 89 ms (21.07%), Space: 14 MB (37.23%) - LeetHub

---
+++ 415-add-strings/415-add-strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        
+        def str2int(num):
+            numDict = {'0' : 0, '1' : 1, '2' : 2, '3' : 3, '4' : 4, '5' : 5,
+                  '6' : 6, '7' : 7, '8' : 8, '9' : 9}
+            output = 0
+            for d in num:
+                output = output * 10 + numDict[d]
+            return output
+        
+        return str(str2int(num1) + str2int(num2)) 
