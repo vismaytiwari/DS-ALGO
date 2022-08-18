d24f89dbf5253b725698e273653ce8e537a8ac59
vismaytiwari <89745751+vismaytiwari@users.noreply.github.com>
2022-08-18 22:38:31 +0530

    Time: 42 ms (80.85%), Space: 14 MB (29.35%) - LeetHub

---
+++ 86-partition-list/86-partition-list.py
@@ -0,0 +1,18 @@
+class Solution:
+    def partition(self, head, x):
+        left_head = ListNode(None)  # head of the list with nodes values < x
+        right_head = ListNode(None)  # head of the list with nodes values >= x
+        left = left_head  # attach here nodes with values < x
+        right = right_head  # attach here nodes with values >= x
+        # traverse the list and attach current node to left or right nodes
+        while head:
+            if head.val < x:
+                left.next = head
+                left = left.next
+            else:  # head.val >= x
+                right.next = head
+                right = right.next
+            head = head.next
+        right.next = None  # set tail of the right list to None
+        left.next = right_head.next  # attach left list to the right
+        return left_head.next  # head of a new partitioned list
